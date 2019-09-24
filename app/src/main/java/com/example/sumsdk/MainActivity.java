ef69ed75b8b603bd46b438d10d30c8d0f53a1ef6
sandeep <sandeep@ashmar.in>
2019-09-24 14:17:08 +0530

    add

---
+++ app/src/main/java/com/example/sumsdk/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.sumsdk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.sumlib.NumSum;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ sumlib/src/main/java/com/example/sumlib/NumSum.java
@@ -0,0 +1,10 @@
+package com.example.sumlib;
+
+import android.widget.Toast;
+
+public class NumSum {
+    public NumSum(int x, int y){
+        int z=x+y;
+
+    }
+}
